232dd8924f8b3e001533f0d07d65c4e656fa575d
Camilo López <camiloj.lopez79@gmail.com>
2024-09-24 23:42:08 -0300

    Commit 1

--- src/appgym/AppGym.java
+++ src/appgym/AppGym.java
@@ -15,7 +15,7 @@ public class AppGym {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Test Prueba de git");
         
     }
-    
 }
